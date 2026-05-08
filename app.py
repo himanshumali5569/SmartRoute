c59a2288592787069db913457ebc43c66900d707
nehearthimansh-star <nehearthimansh@gmail.com>
2026-05-08 11:00:20 +0530

    Com

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
+    database_url = os.getenv("DATABASE_URL")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
     @app.route("/")
     def home():
-        return "Flask running on Vercel"
+        return "Database Connected"
 
     return app
