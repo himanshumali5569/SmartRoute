53cd4cf24c2b882fb70e01161f26bab0fd6586c8
nehearthimansh-star <nehearthimansh@gmail.com>
2026-05-08 11:04:21 +0530

    Com

--- app.py
+++ app.py
@@ -7,18 +7,15 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    database_url = os.getenv("DATABASE_URL")
-
-    if not database_url:
-        return app
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    db.init_app(app)
-
     @app.route("/")
     def home():
-        return "Database Connected"
+        return "Flask + SQLAlchemy Working"
+
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        db.init_app(app)
 
     return app
